219e3520c506f77c8c9b6907515eed40a94ddceb
Fahad Shah <39241328+1FahadShah@users.noreply.github.com>
2025-07-02 21:22:44 +0530

    Create 01_calculator.py

---
+++ course-01-getting-started-with-python/01_calculator.py
@@ -0,0 +1,22 @@
+#using function to perform basic calculator operations
+
+def operation(a,b,op):
+    if (op == '+'):
+        print(f"Addition: \n {a} + {b} = {a+b}")
+    elif (op == '-'):
+        print(f"Subtraction: \n {a} - {b} = {a-b}")
+    elif (op == '*'):
+        print(f"Multiplication: \n {a} * {b} = {a*b}")
+    elif (op == '/'):
+        if b == 0:
+            print("Error!!! Division by Zero")
+        else:
+            print(f"Multiplication: \n {a} / {b} = {a/b}")
+    else:
+        print("Unknown Operation")
+    
+
+a = float(input("Enter First Number: "))
+op = input("Enter operation to perform (+, -, *, /): ")
+b = float(input("Enter Second Number: "))
+operation(a,b,op)
