4df3c77e7283d17327de3d6251e1d0fa7d8c252e
Fahad Shah <39241328+1FahadShah@users.noreply.github.com>
2025-07-30 21:34:54 +0530

    added summary fetch logic for Wikipedia

--- course-05-capstone-project/crawler/fetch.py
+++ course-05-capstone-project/crawler/fetch.py
@@ -21,6 +21,47 @@ def fetch_summary(topic):
             data = response.read()
             json_data = json.loads(data)
 
+            summary = json_data.get("Abstract", "").strip()
+            url = json_data.get("AbtractURL", "").strip()
+
+            if summary:
+                return {
+                    "topic": topic,
+                    "summary": summary,
+                    "source": "DuckDuckGo",
+                    "source_url": url
+                }
 
     except Exception as e:
         print('DuckDuckGo fetch Failed', e)
+
+    return None
+
+
+
+    #If DuckDuckGo gave us nothing, we will try Wikipedia
+    try:
+        wiki_base_url = "https://en.wikipedia.org/api/rest_v1/page/summary/"
+        query_string = urllib.parse.encode(topic)
+        wiki_url = wiki_base_url + query_string
+
+        with urllib.request.urlopen(wiki_url) as response:
+            data = response.read()
+            json_data = json.loads(data)
+
+            summary = json_data.get("extract", "").strip()
+            url = json_data.get("content_urls", {}).get("desktop", {}).get("page", "")
+
+            if summary:
+                return {
+                "topic": topic,
+                "summary": summary,
+                "source": Wikipedia,
+                "source_url":url
+            }
+
+
+    except Exception as e:
+        print("Wikipedia fetch failed", e)
+
+    return None
