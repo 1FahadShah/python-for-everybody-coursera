abb9d392ddcadb757f7cc6fc85ab50268bcbcc23
Fahad Shah <39241328+1FahadShah@users.noreply.github.com>
2025-07-31 02:24:38 +0530

    Added function in main.py
    Added function to main.py to fetch summaries for each topics in text file

---
+++ course-05-capstone-project/main.py
@@ -0,0 +1,47 @@
+import sys
+import os
+from crawler.fetch import fetch_summary
+from crawler.db_writer import create_table, insert_summary
+from datetime import datetime
+
+def crawl_topics(topics_file_path):
+    """
+    this function reads topics from a text file
+    and fetches summaries for each of them.
+    """
+
+    if not os.path.exists(topics_file_path):
+        print("File not found:", topics_file_path)
+        return
+
+    create_table()
+
+    topics = []
+
+    # this will get topics from file and append it to 'topics' list
+    with open(topics_file_path, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line != "":
+                topics.append(line)
+
+    for topic in topics:
+        print("Fetching Summary for: ",topic)
+        result = fetch_summary(topic)
+
+        if result:
+            result["created_at"] = datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+            insert_summary(result)
+            print(f"Summary for {topic} saved in DB")
+        else:
+            print("No Summary found for:", topic)
+
+
+#manage inputs from CLI
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py data/topics.txt")
+    else:
+        topics_file = sys.argv[1]
+        crawl_topics(topics_file)
