c83a81ec5affd37459031693f26277dce662868d
Fahad Shah <39241328+1FahadShah@users.noreply.github.com>
2025-07-31 03:50:07 +0530

    Added few lines of code for summary logic

--- course-05-capstone-project/main.py
+++ course-05-capstone-project/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from crawler.fetch import fetch_summary
-from crawler.db_writer import create_table, insert_summary
+from crawler.db_writer import create_table, insert_summary, save_summary_to_file
 from crawler.utils import get_timestamp
 from datetime import datetime
 
@@ -33,9 +33,12 @@ def crawl_topics(topics_file_path):
         if result:
             result["created_at"] = get_timestamp()
             insert_summary(result)
+            save_summary_to_file(result)
             print(f"Summary for {topic} saved in DB")
         else:
             print("No Summary found for:", topic)
+            with open("failed_topics.txt","a",encoding="utf-8") as fail_log:
+                fail_log.write(topic + "\n")
 
 
 #manage inputs from CLI
